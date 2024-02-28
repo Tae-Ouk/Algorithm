4141c06ff9a2ce301e6a48918cb79ff5bc9fb9dc
Tae-Uk <69581725+Tae-Ouk@users.noreply.github.com>
2024-02-28 10:08:26 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 109.69 ms, Memory: 23.2 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.swift
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.swift
@@ -2,12 +2,22 @@ import Foundation
 
 func solution(_ n:Int) -> Int {
     var arr: [Int] = []
+    var ans: [Int] = []
     
+    for i in 1...n-1 {
+        arr.append(n % i == 1 ? i : 0)
+    }
+    
+    ans = arr.filter{ $0 != 0 }
+    
+    
+    /*
     for i in 1...n-1 {
         if n % i == 1 {
             arr.append(i)
         }
     }
+    */
     
-    return arr[0]
+    return ans[0]
 }
