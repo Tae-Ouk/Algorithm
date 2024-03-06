ebfbcf9d935e8df5e55f9e01bff8262825b0e1a3
Tae-Uk <69581725+Tae-Ouk@users.noreply.github.com>
2024-03-06 12:56:43 +0900

    [level 1] Title: 음양 더하기, Time: 0.03 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.swift
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.swift
@@ -4,15 +4,8 @@ func solution(_ absolutes:[Int], _ signs:[Bool]) -> Int {
     var ans: Int = 0
     
     for i in 0...absolutes.count-1 {
-        if signs[i] == true {
-            ans += absolutes[i]
-        }
-        else {
-            ans -= absolutes[i]
-        }        
+       (signs[i] == true) ? (ans += absolutes[i]) : (ans -= absolutes[i])
     }
-    
-    print(ans)
     
     return ans
 }
