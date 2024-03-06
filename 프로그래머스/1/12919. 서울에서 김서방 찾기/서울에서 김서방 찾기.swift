02893f5d04f10a82569b91e6464c7d17e13df3a5
Tae-Uk <69581725+Tae-Ouk@users.noreply.github.com>
2024-03-06 09:17:39 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.10 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.swift
@@ -0,0 +1,9 @@
+func solution(_ seoul:[String]) -> String {
+    var pos = seoul.firstIndex(of: "Kim")!
+    
+    
+    //if var position = pos { }
+    //print("김서방은 \(pos)에 있다")
+    
+    return "김서방은 \(pos)에 있다"
+}
