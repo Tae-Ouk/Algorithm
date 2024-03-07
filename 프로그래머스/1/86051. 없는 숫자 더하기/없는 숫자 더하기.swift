afd79d2c196b1b31dbdd8ef6bf68dc731f923850
Tae-Uk <69581725+Tae-Ouk@users.noreply.github.com>
2024-03-07 09:56:06 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.09 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.swift
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.swift
@@ -1,11 +1,8 @@
 import Foundation
 
 func solution(_ numbers:[Int]) -> Int {
-    var ans: Int = 0
-    
-    for i in 0...9 {
-       ans += numbers.contains(i) != true ? i : 0
-    }
+    var arr = (0...9).filter{ !numbers.contains($0) }
+    var ans = arr.reduce(0, +)
     
     return ans
 }
