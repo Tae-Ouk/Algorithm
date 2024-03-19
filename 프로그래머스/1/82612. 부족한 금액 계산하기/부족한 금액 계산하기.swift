59c9a280071fd2dcfae919c3bc6d555a7c7ee91c
Tae-Uk <69581725+Tae-Ouk@users.noreply.github.com>
2024-03-19 09:50:38 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+func solution(_ price:Int, _ money:Int, _ count:Int) -> Int64{
+    var val = (1...count).map{ $0 * price }.reduce(0, +)
+    
+    return Int64(val > money ? val - money : 0)
+    
+}
